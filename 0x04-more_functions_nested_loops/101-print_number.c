1452e374f090c49e9cac0b78ee415182a58a0578
aycrown1 <ayoadeadedamola198@gmail.com>
2023-02-24 17:16:35 -0800

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,23 +6,16 @@
   * @n: The integer to prints.
   *
   */
-
 void print_number(int n)
 {
-int divisor = 1;
-
-if (n < 0)
-{
-	_putchar('-');
-	n = -n;
-};
-while (n / divisor >= 10)
-{
-	divisor *= 10;
-};
-while (divisor != 0)
-{
-	_putchar((n / divisor) % 10 + '0');
-	divisor /= 10;
-};
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
