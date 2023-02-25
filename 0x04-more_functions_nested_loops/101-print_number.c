e0d49c5ea9ac88c84010968358e43a45822b362d
aycrown1 <ayoadeadedamola198@gmail.com>
2023-02-24 16:18:57 -0800

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,22 +6,30 @@
   * @n: The integer to prints.
   *
   */
+
 void print_number(int n)
 {
 	int divisor = 1;
 
 	if (n < 0)
 	{
-		_putchar('-');
+		/*Handle negative numbers*/
+		putchar('-');
 		n = -n;
-	}
+	};
 	while (n / divisor >= 10)
+		/**
+		 * Finding the power of 10 that is dibvisible
+		 * and less than by n to get the first digit
+		 */
 	{
 		divisor *= 10;
-	}
+	};
 	while (divisor != 0)
+		/* To control the divisor when it 0, then print the last digit*/
 	{
-		_putchar((n / divisor) % 10 + '0');
+		putchar((n / divisor) % 10 + '0');
 		divisor /= 10;
-	}
+		/* Decreases the power of 10, after a digit is printed*/
+	};
 }
