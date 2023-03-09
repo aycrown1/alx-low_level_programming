86decc5c7fee38ee1c57a21ffc56a4a0dcb231b0
aycrown1 <ayoadeadedamola198@gmail.com>
2023-03-09 03:37:40 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,16 +14,16 @@ int _sqrt_recursion(int n)
 
 	if (n == 0 || n == 1)
 	{
-		return n;
+		return (n);
 	}
 	root = _sqrt_recursion(n / 4) * 2;
 	if (root * root <= n && (root + 1) * (root + 1) > n)
 	{
-		return root;
+		return (root);
 	}
 	if (root * root < n)
 	{
-		return root + 1;
+		return (root + 1);
 	}
-	return -1;
+	return (-1);
 }
