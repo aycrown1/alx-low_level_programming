b13ad171b193d2f77a0740b9c6a9bbb346829b94
aycrown1 <ayoadeadedamola198@gmail.com>
2023-03-20 19:53:14 -0700

    Canis lupus familiaris

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,18 +18,18 @@ struct dog
 
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 /**
  * dog_t - defining  a new name for struct dog
  *
  */
 
-typedef struct dog dog_t;
+typedef struct dog dog_t;
 
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
 
