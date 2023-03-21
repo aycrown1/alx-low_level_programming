e9f539d84e779f501e309af63c4c8f0afe887a1e
aycrown1 <ayoadeadedamola198@gmail.com>
2023-03-20 18:31:55 -0700

    SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File name - 4-sum.h
+ * macro name - SUM(x, y)
+ * usage - computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
