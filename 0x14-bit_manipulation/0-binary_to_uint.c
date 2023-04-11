112994d5840d4d50c69044447258106de5552c5b
aycrown1 <ayoadeadedamola198@gmail.com>
2023-04-10 22:06:57 -0700

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,31 @@
 #include "main.h"
 
+
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to print in binary.
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: The converted number, or 0 if there is one or more chars in
+ *         the string b that is not 0 or 1, or if b is NULL.
  */
-void print_binary(unsigned long int n)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned long int mask = 1;
-	int i;
+	unsigned int num = 0;
+	int i = 0;
 
-	for (i = 0; i < 63; i++)
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
 	{
-		if ((n >> i) == 0)
-			break;
+		if (b[i] == '0' || b[i] == '1')
+			num = (num << 1) + (b[i] - '0');
+		else
+			return (0);
+
+		i++;
 	}
 
-	while (i >= 0)
-	{
-		_putchar((n >> i) & mask ? '1' : '0');
-		i--;
-	}
+	return (num);
 }
 
