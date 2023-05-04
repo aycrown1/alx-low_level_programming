bf205ea5cd2f74fac973b267a353579a6e0246a3
aycrown1 <ayoadeadedamola198@gmail.com>
2023-05-04 14:42:11 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,37 @@
-#include "main.h"
 #include <string.h>
+#include "main.h"
+
+/**
+ * _recursion - takes a pointer to a null-terminated string, 
+ *		a starting index, and an ending index
+ * @start: a starting index that delimit a substring of the input string s. 
+ * @end: an ending index that delimit a substring of the input string s.
+ * Return: returns 1 if the substring is a palindrome, and 0 if it is not.
+ */
+int _recursion(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return 1;
+	}
+	if (s[start] != s[end])
+	{
+		return 0;
+	}
+	return _recursion(s, start + 1, end - 1);
+}
+
 /**
  * is_palindrome - Checks if a string is a palindrome
  * @s: The string to check
  *
  * Return: 1 if s is a palindrome, otherwise return 0
  */
-
 int is_palindrome(char *s)
 {
-	int i;
 	int len = strlen(s);
-	
-	for (i = 0; i < len / 2; i++)
-	{
-		if (s[i] != s[len - 1 - i])
-		{
-			return (0);
-		}
-	}
-	return (1);
+
+	return _recursion(s, 0, len - 1);
 }
+
 
