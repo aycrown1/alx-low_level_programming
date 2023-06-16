553aab31d47e55fab62cbc13ee6e92c020b66c43
aycrown1 <ayoadeadedamola198@gmail>
2023-06-16 23:24:50 +0100

    Palindromes and Creakme4

---
+++ 0x17-doubly_linked_lists/c.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int isPalindrome(int num) {
+    int reversed = 0;
+    int original = num;
+    
+    while (num > 0) {
+        int remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+    
+    return reversed == original;
+}
+
+int main() {
+    int largestPalindrome = 0;
+    int i, j;
+    
+    for (i = 999; i >= 100; i--) {
+        for (j = 999; j >= 100; j--) {
+            int product = i * j;
+            if (isPalindrome(product) && product > largestPalindrome) {
+                largestPalindrome = product;
+            }
+        }
+    }
+    FILE *file = fopen("102-result", "w");
+    fprintf(file, "%d", largestPalindrome);
+    fclose(file);
+    
+    return 0;
+}
